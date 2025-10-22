b75d474fc944061d95d449368a8506f72b2b28e7
CoyD1 <apranovich22010kostya@mail.ru>
2025-10-22 23:54:41 +0700

    Добавлены API эндпоинты для категорий и транзакций

--- backend/finance/views.py
+++ backend/finance/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Category, Transaction, UserSavingGoal
+from rest_framework import viewsets, permissions
+from .serializers import CategorySerializer, TransactionSerializer, UserSavingGoalSerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
+    
+    def preform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class USerSavingGoalViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSavingGoalSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return UserSavingGoal.objects.filter(user=self.request.user)
+        
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
